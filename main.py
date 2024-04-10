2457711b81d499b5eeb8a9a5a515c91a70e6e3da
kc <chitranshi@zib.de>
2024-04-10 17:46:05 +0200

    Added training transforms for the models

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch
 from models import *
 from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets
+from torchvision import transforms
 from torch.optim import Adam, Adagrad, SGD
 
 if __name__ == "__main__":
@@ -34,6 +35,47 @@ if __name__ == "__main__":
         raise ValueError(f"Can't use model {args.model} for dataset {args.dataSet}") 
     #-------------------------------- Reading Data --------------------------------#
     
+    modelTrainingTransforms = {
+
+        'VGG19': transforms.Compose([
+                    transforms.Resize ( (150 , 150) ),
+                    transforms.RandomHorizontalFlip(p=0.5),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+                ]),
+        
+        'ResNet50': transforms.Compose([
+                        transforms.Resize(256),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomVerticalFlip(),
+                        transforms.RandomRotation(degrees=45),
+                        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+                        transforms.CenterCrop(224),
+                        transforms.ToTensor(),
+                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    ]),
+        
+        'ResNet20':  transforms.Compose([
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomCrop(32, 4),
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                std=[0.229, 0.224, 0.225]),
+                    ]),
+        
+        'WideResNet': transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                std=[0.229, 0.224, 0.225])  
+                    ]),
+        
+        'BasicCNN': transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                std=[0.229, 0.224, 0.225])  
+                    ]) 
+    }
+
     training_data = datasets.MNIST(
         root="data",
         train=True,
@@ -49,6 +91,8 @@ if __name__ == "__main__":
     ) 
 
     #-------------------------------- Intializing the Data Loader --------------------------------#
+
+    
 
     batchSize=100
     dataLoader=DataLoader()
